a97ca898b006ddbf1ab9cecb68efb53bd66c3479
donm <donm@nvidia.com>
2017-09-28 18:25:28 +0800

    fix the error

--- src/main/java/spring/demo/service/DemoService.java
+++ src/main/java/spring/demo/service/DemoService.java
@@ -3,7 +3,7 @@ package spring.demo.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public lass DemoService {
+public class DemoService {
 
 	public String Verify(String username,String password){
 		
